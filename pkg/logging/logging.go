d795868f5716e9dd991a3ffa248f9169a995e5f3
2lovecode <liu546hao@163.com>
2020-03-02 16:18:08 +0800

    edit

--- pkg/logging/logging.go
+++ pkg/logging/logging.go
@@ -1,30 +1,25 @@
 package logging
 
-type Logging struct {
+import (
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"os"
+	"t-blog-back/pkg/setting"
+)
+
+var Tlog = logrus.New()
+
+func init() {
+	appCgf, err := setting.Cfg.GetSection("app")
+	if err != nil {
+		fmt.Println("error")
+	} else {
+		logFile, err := os.Open(appCgf.Key("RUNTIME_PATH").MustString("./runtime/log"))
+		if err == nil {
+			Tlog.SetOutput(logFile)
+		}
+	}
 }
 
-func (log *Logging) Trace() {
-}
-
-func (log *Logging) Debug() {
-}
-
-func (log *Logging) Info() {
-}
-
-func (log *Logging) Warn() {
-}
-
-func (log *Logging) Error() {
-}
-
-func (log *Logging) Crit() {
-}
-
-func (log *Logging) Alert() {
-}
-
-func (log *Logging) Emer() {
-}
 
 
